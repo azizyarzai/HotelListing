a3845c2ad1e2b5d9f20bebd8f17a77147e49cc28
azizyarzai <azizulrahman.yarzai@gmail.com>
2021-06-14 18:52:48 +0430

    Added Database, AutoMapper, Unit Of Work and DTOs.

---
+++ HotelListing/Migrations/20210613143415_First Migration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hotels_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Afghanistan", "AF" },
+                    { 2, "India", "In" },
+                    { 3, "Iran", "IR" },
+                    { 4, "United States Of America", "US" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Kabul", 1, "Sandals Resort and Spa", 4.5 },
+                    { 2, "George Town", 2, "Comfort Suites", 4.2999999999999998 },
+                    { 3, "Nassu", 3, "Grand Palldium", 4.0 },
+                    { 4, "Kabul", 4, "Dubai Marina", 3.5 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_CountryId",
+                table: "Hotels",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
---
+++ HotelListing/Models/DatabaseContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options): base(options)
+        {
+
+        } 
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData
+                (
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Afghanistan",
+                        ShortName = "AF"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "India",
+                        ShortName = "In"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Iran",
+                        ShortName = "IR"
+                    },
+                    new Country
+                    {
+                        Id = 4,
+                        Name = "United States Of America",
+                        ShortName = "US"
+                    }
+                );
+
+            builder.Entity<Hotel>().HasData
+                (
+                    new Hotel
+                    {
+                        Id = 1,
+                        Name = "Sandals Resort and Spa",
+                        Address = "Kabul",
+                        CountryId = 1,
+                        Rating = 4.5
+                    },
+                    new Hotel
+                    {
+                        Id = 2,
+                        Name = "Comfort Suites",
+                        Address = "George Town",
+                        CountryId = 2,
+                        Rating = 4.3
+                    },
+                    new Hotel
+                    {
+                        Id = 3,
+                        Name = "Grand Palldium",
+                        Address = "Nassu",
+                        CountryId = 3,
+                        Rating = 4
+                    },
+                    new Hotel
+                    {
+                        Id = 4,
+                        Name = "Dubai Marina",
+                        Address = "Kabul",
+                        CountryId = 4,
+                        Rating = 3.5
+                    }
+                );
+        }
+    }
+}
